4c0aa29add5a70303739d3edcf74282191fe2293
Diego Salgado <dasalgadob@unal.edu.co>
2019-04-24 16:35:02 -0500

    openmp hello world added

---
+++ computacion_paralela/openmp/hello_omp.c
@@ -0,0 +1,10 @@
+#include "omp.h"
+
+void main(){
+  #pragma omp parallel
+  {
+    int ID = omp_get_thread_num();
+    printf("Hello (%d)", ID);
+    printf("World (%d) \n" , ID);
+  }//End parallel region
+}
