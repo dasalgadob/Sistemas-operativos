19de7fe00a9f792ed3d00921346a4effd0c29d06
Diego Salgado <dasalgadob@unal.edu.co>
2019-05-02 18:42:29 -0500

    Version that has problems with several threads, it needs to copy a temp file

--- computacion_paralela/practica1/test_pnglib.c
+++ computacion_paralela/practica1/test_pnglib.c
@@ -34,7 +34,6 @@ void abort_(const char * s, ...)
         abort();
 }
 
-int x, y;
 int kernel_size;
 int width, height;
 png_byte color_type;
@@ -107,12 +106,85 @@ void read_png_file(char* file_name)
                 abort_("[read_png_file] Error during read_image");
 
         row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-        row_pointers2 = (png_bytep*) malloc(sizeof(png_bytep) * height);
-        for (y=0; y<height; y++)
+        //row_pointers2 = (png_bytep*) malloc(sizeof(png_bytep) * height);
+        for (int y=0; y<height; y++)
                 row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
-                row_pointers2[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+                //row_pointers2[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
 
         png_read_image(png_ptr, row_pointers);
+        //png_read_image(png_ptr, row_pointers2);
+
+        fclose(fp);
+}
+
+void read_png_file2(char* file_name)
+{
+        char header[8];    // 8 is the maximum size that can be checked
+
+        /* open file and test for it being a png */
+
+
+        FILE *fp = fopen(file_name, "rb");
+        if (!fp)
+                abort_("[read_png_file] File %s could not be opened for reading", file_name);
+        fread(header, 1, 8, fp);
+        if (png_sig_cmp(header, 0, 8))
+                abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+
+
+        /* initialize stuff */
+        png_ptr2 = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+        if (!png_ptr2)
+                abort_("[read_png_file] png_create_read_struct failed");
+
+        info_ptr2 = png_create_info_struct(png_ptr2);
+        if (!info_ptr2)
+                abort_("[read_png_file] png_create_info_struct failed");
+
+        if (setjmp(png_jmpbuf(png_ptr2)))
+                abort_("[read_png_file] Error during init_io");
+
+
+        png_init_io(png_ptr2, fp);
+        png_set_sig_bytes(png_ptr2, 8);
+        //Duplicate
+        //png_init_io(png_ptr2, fp);
+        //png_set_sig_bytes(png_ptr2, 8);
+
+        png_read_info(png_ptr2, info_ptr2);
+        //png_read_info(png_ptr2, info_ptr2);
+
+        width = png_get_image_width(png_ptr2, info_ptr2);
+        height = png_get_image_height(png_ptr2, info_ptr2);
+        color_type = png_get_color_type(png_ptr2, info_ptr2);
+        bit_depth = png_get_bit_depth(png_ptr2, info_ptr2);
+        printf("bit_depth: %d\n",bit_depth );
+        number_of_passes = png_set_interlace_handling(png_ptr2);
+        //Duplicate
+        /*
+        width = png_get_image_width(png_ptr2, info_ptr2);
+        height = png_get_image_height(png_ptr2, info_ptr2);
+        color_type = png_get_color_type(png_ptr2, info_ptr2);
+        bit_depth2 = png_get_bit_depth(png_ptr2, info_ptr2);
+        printf("bit_depth: %d\n",bit_depth );
+        number_of_passes2 = png_set_interlace_handling(png_ptr2);
+        */
+        png_read_update_info(png_ptr2, info_ptr2);
+        //png_read_update_info(png_ptr2, info_ptr2);
+
+
+        /* read file */
+        if (setjmp(png_jmpbuf(png_ptr2)))
+                abort_("[read_png_file] Error during read_image");
+
+        //row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+        row_pointers2 = (png_bytep*) malloc(sizeof(png_bytep) * height);
+        for (int y=0; y<height; y++)
+                //row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+                row_pointers2[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr2,info_ptr2));
+
+        png_read_image(png_ptr2, row_pointers2);
         //png_read_image(png_ptr, row_pointers2);
 
         fclose(fp);
@@ -168,9 +240,65 @@ void write_png_file(char* file_name)
         png_write_end(png_ptr, NULL);
 
         /* cleanup heap allocation */
-        for (y=0; y<height; y++)
+        for (int y=0; y<height; y++)
                 free(row_pointers[y]);
         free(row_pointers);
+
+        fclose(fp);
+}
+
+void write_png_file2(char* file_name)
+{
+        /* create file */
+        FILE *fp = fopen(file_name, "wb");
+        if (!fp)
+                abort_("[write_png_file] File %s could not be opened for writing", file_name);
+
+
+        /* initialize stuff */
+        png_ptr2 = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+        if (!png_ptr2)
+                abort_("[write_png_file] png_create_write_struct failed");
+
+        info_ptr2 = png_create_info_struct(png_ptr2);
+        if (!info_ptr2)
+                abort_("[write_png_file] png_create_info_struct failed");
+
+        if (setjmp(png_jmpbuf(png_ptr2)))
+                abort_("[write_png_file] Error during init_io");
+
+        png_init_io(png_ptr2, fp);
+
+
+        /* write header */
+        if (setjmp(png_jmpbuf(png_ptr2)))
+                abort_("[write_png_file] Error during writing header");
+
+        png_set_IHDR(png_ptr2, info_ptr2, width, height,
+                     bit_depth, color_type, PNG_INTERLACE_NONE,
+                     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+        png_write_info(png_ptr2, info_ptr2);
+
+
+        /* write bytes */
+        if (setjmp(png_jmpbuf(png_ptr2)))
+                abort_("[write_png_file] Error during writing bytes");
+
+        png_write_image(png_ptr2, row_pointers);
+
+
+        /* end write */
+        if (setjmp(png_jmpbuf(png_ptr2)))
+                abort_("[write_png_file] Error during end of write");
+
+        png_write_end(png_ptr2, NULL);
+
+        /* cleanup heap allocation */
+        //for (y=0; y<height; y++)
+        //        free(row_pointers[y]);
+        //free(row_pointers);
 
         fclose(fp);
 }
@@ -188,9 +316,9 @@ void *process_file(void *param)
                  PNG_COLOR_TYPE_RGBA, png_get_color_type(png_ptr, info_ptr));
 
   //Iterar sobre las filas del hilo
-  for (y=myPair->i; y<myPair->j; y++) {
+  for (int y=myPair->i; y<myPair->j; y++) {
           //png_byte* row = row_pointers[y];
-          for (x=0; x<width-myPair->k+1; x++) {
+          for (int x=0; x<width-myPair->k+1; x++) {
             double rsum=0, gsum=0, bsum=0, asum=0;
             //Hacer la convolucion
             //Primero iteral sobre las filas de la convolucion
@@ -211,10 +339,15 @@ void *process_file(void *param)
             //Asignar valor a posicion de la convolucion
             png_byte* row_change = row_pointers[y + (myPair->k/2)];
             png_byte* position = &(row_change[(x+ (myPair->k/2))*4]);
-            matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][0] = (int) rsum;
-            matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][1] = (int) gsum;
-            matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][2] = (int) bsum;
-            matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][3] = (int) asum;
+            position[0] = (int) rsum;
+            position[1] = (int) gsum;
+            position[2] = (int) bsum;
+            position[3] = (int) asum;
+
+            //matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][0] = (int) rsum;
+            //matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][1] = (int) gsum;
+            //matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][2] = (int) bsum;
+            //matrix_result[y + (myPair->k/2)][(x+ (myPair->k/2))][3] = (int) asum;
                   //printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
                   //       x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
 
@@ -236,9 +369,9 @@ void process_file_join(void)
                 abort_("[process_file] color_type of input file must be PNG_COLOR_TYPE_RGBA (%d) (is %d)",
                        PNG_COLOR_TYPE_RGBA, png_get_color_type(png_ptr, info_ptr));
 
-        for (y=0; y<height; y++) {
+        for (int y=0; y<height; y++) {
                 png_byte* row = row_pointers[y];
-                for (x=0; x<width; x++) {
+                for (int x=0; x<width; x++) {
                         png_byte* ptr = &(row[x*4]);
                         //printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
                         //       x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
@@ -289,6 +422,8 @@ int main(int argc, char **argv)
   preprocessing();
 
   read_png_file(argv[1]);
+  //write_png_file("temp.png");
+  //read_png_file2("temp.png");
 
   kernel_size = atoi(argv[3]);
   n_threads= atoi(argv[4]);
@@ -320,7 +455,7 @@ int main(int argc, char **argv)
   pthread_join(tid[i], NULL);
   }
 
-  process_file_join();
+  //process_file_join();
 
   //process_file();
   write_png_file(argv[2]);
